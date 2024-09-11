91cc7e471aa76d5efb53d2d3727112ad7af60aa7
Silas Boutwell <silaslee.b@gmail.com>
2024-09-11 08:12:32 -0700

    Changed the layout a bit

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import Nav from './components/navigation'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.nav}>
-        <Text style={styles.title}>God And I Time</Text>
-      </View>
-      <View style={styles.body}>
-        <Text style={styles.bodyTitle}>This is the body area.</Text>
-        <Text style={styles.content}>Welcome to your daily devotion!</Text>
-      </View>
+      <Nav />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {flex: 1, marginHorizontal: 50},
-  nav: {padding: 10, backgroundColor: '#ccc', },
-  title: {fontSize: 30, fontWeight: 'semibold'},
-  body: {flex: 1, alignItems: 'center',},
-  bodyTitle: {},
-  content: {},
 });
